e0377f15daecf7df86bded3760323f2750002fda
11121997 <nawalkhaled27@yahoo.com>
2023-12-08 08:35:28 -0800

    up

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import json
 from os.path import isfile
-from models.base_model import BaseModel
-from models.user import User
 
 
 classnames = ['BaseModel', 'User', 'State', 'City', 'Place', 'Review', 'Amenity']
