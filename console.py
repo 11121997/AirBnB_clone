62194910c5dc261f424bae7db9f3289d9afae4fc
amer2040 <aboodamer7@gmail.com>
2023-12-09 23:32:07 +0200

    remove console help doc

--- console.py
+++ console.py
@@ -24,24 +24,14 @@ class HBNBconsole(cmd.Cmd):
         'Review': Review
     }
 
-    def do_quit(self, arg):
-        '''This command to exit from program'''
-        return True
-    # alias do_quit
-    do_exit = do_quit
-
-    def help_quit(self):
-        '''Documentation for quit help command'''
-        print('This command to to exit from the app\n')
-
     def do_EOF(self, arg):
         '''Handle EOF to End program'''
         print()  # emtpy line before exit
         return True
 
-    def help_EOF(self, arg):
-        '''Documentation for EOF help command'''
-        print('Exit program when type CTRL+D')
+    def do_quit(self, arg):
+        '''This command to exit from program'''
+        return True
 
     def emptyline(self):
         '''empty input line'''
@@ -58,10 +48,6 @@ class HBNBconsole(cmd.Cmd):
         new_inst = eval(arg)()
         storage.save()
         print(new_inst.id)
-
-    def help_create(self):
-        '''Documentation for create help command'''
-        print('[Usage]: create <classname> to create a class')
 
     def do_show(self, arg):
         """Prints the string representation of
@@ -84,10 +70,6 @@ class HBNBconsole(cmd.Cmd):
         else:
             print("** no instance found **")
 
-    def help_show(self):
-        '''Documentation for show help command'''
-        print('[Usage]: show <classname> <id>\n')
-
     def do_destroy(self, arg):
         '''Delete instance by classname and id'''
         if not arg:
@@ -109,10 +91,6 @@ class HBNBconsole(cmd.Cmd):
         else:
             print("** no instance found **")
 
-    def help_destroy(self):
-        '''Documentation for destroy help command'''
-        print('[Usage]: destroy <classname> <id>\n')
-
     def do_all(self, arg):
         '''Prints all string representation of
         all instances based or not on the class name'''
@@ -132,9 +110,6 @@ class HBNBconsole(cmd.Cmd):
         else:
             print(A_list)
 
-    def help_all(self):
-        """help for all func"""
-        print("'[Usage]: all <classname> <id>\n'")
 
     def do_update(self, arg):
         """Updates an instance by class name and id."""
@@ -173,10 +148,6 @@ class HBNBconsole(cmd.Cmd):
         setattr(inst, attr_name, eval(attr_val))
         # saves after updates
         inst.save()
-
-    def help_update(self):
-        '''Documentation for update help command'''
-        print("Usage: update <className> <id> <attrName> <attrVal>\n")
 
 
 if __name__ == '__main__':
